979e787dc193184fdb6e8655a3f09dbdf45cdf5b
harishfalco <harishfalcon400@gmail.com>
2021-11-28 11:30:42 +0530

    production relase

---
+++ index.js
@@ -0,0 +1,47 @@
+const express = require("express");
+const format = require("date-format")
+const app = express()
+
+const PORT = 4000 || process.env.PORT
+
+app.get("/api/v1/:token",(req,res)=>{
+    console.log(req.params.token);
+    res.status(200).json({params:req.params.token})
+})
+
+app.get("/api/v1/instagram",(req,res)=>{
+    const instaSocial = {
+        username:"harish",
+        followers:100,
+        follows:55,
+        date:format.asString("dd:MM:yy - hh:mm:ss",new Date())
+    }
+
+    res.json({instaSocial});
+})
+
+app.get("/api/v1/facebook",(req,res)=>{
+    const facebookSocial = {
+        username:"harish07",
+        followers:1000,
+        follows:551,
+        date:format.asString("dd:MM:yy - hh:mm:ss",new Date())        
+    }
+
+    res.json({facebookSocial});
+})
+
+app.get("/api/v1/linkedin",(req,res)=>{
+    const linkedSocial = {
+        username:"harish",
+        followers:500,
+        follows:135,
+        date:format.asString("dd:MM:yy - hh:mm:ss",new Date())
+
+    }
+    res.json({linkedSocial});
+})
+
+app.listen(PORT,()=>{
+    console.log("listening");
+})
