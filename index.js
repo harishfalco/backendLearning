402e30868b7e5563ef1230f3dbef911a96e09dfe
harishfalco <harishfalcon400@gmail.com>
2021-11-28 11:46:57 +0530

    port issue fixed

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const format = require("date-format")
 const app = express()
 
-const PORT = 4000 || process.env.PORT
+const PORT = process.env.PORT || 4000 
 
 app.get("/api/v1/:token",(req,res)=>{
     console.log(req.params.token);
